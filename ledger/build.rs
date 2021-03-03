73d157405cfce494a0fb7f232b23c3229c61af74
Sylvain Ribstein <sylvain.ribstein@gmail.com>
2021-03-03 12:16:36 +0100

    add hash to type

--- ledger/build.rs
+++ ledger/build.rs
@@ -1,11 +1,15 @@
 use std::path::PathBuf;
 
 fn main() {
+    // XXX TODO add header to file with "auto-generated"
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed=src/proto/");
     tonic_build::configure()
         .out_dir(PathBuf::from("src/lib/protobuf"))
         .format(true)
+         // XXX TODO can this be automatic for all type in a file ?
+        .type_attribute("gossip.Intent", "#[derive(Hash)]")
+        .type_attribute("gossip.Dkg", "#[derive(Hash)]")
         .compile(&["src/proto/service.proto"], &["src/proto"])
         .unwrap();
 }
--- ledger/src/lib/protobuf/gossip.rs
+++ ledger/src/lib/protobuf/gossip.rs
@@ -1,9 +1,9 @@
-#[derive(Clone, PartialEq, ::prost::Message)]
+#[derive(Hash, Clone, PartialEq, ::prost::Message)]
 pub struct Intent {
     #[prost(string, tag = "1")]
     pub asset: ::prost::alloc::string::String,
 }
-#[derive(Clone, PartialEq, ::prost::Message)]
+#[derive(Hash, Clone, PartialEq, ::prost::Message)]
 pub struct Dkg {
     #[prost(string, tag = "1")]
     pub msg: ::prost::alloc::string::String,
