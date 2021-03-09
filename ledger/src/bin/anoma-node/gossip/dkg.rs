9ccbb0d0a6b6be43202f22b5754fe69b1ffa44bb
Sylvain Ribstein <sylvain.ribstein@gmail.com>
2021-03-09 09:28:47 +0100

    add type for DKG

--- ledger/src/bin/anoma-node/gossip/dkg.rs
+++ ledger/src/bin/anoma-node/gossip/dkg.rs
@@ -1,8 +1,23 @@
+use super::mempool::{IntentId, Mempool};
+use super::network_behaviour::{Behaviour, BehaviourEvent};
 use anoma::protobuf::gossip::Dkg;
+use futures::channel::mpsc::{channel, Receiver, Sender};
 use prost::Message;
 
 pub const TOPIC: &str = "dkg";
 
-pub fn apply(data: Vec<u8>) -> Result<Dkg, prost::DecodeError> {
-    Dkg::decode(&data[..])
+pub struct DKG {}
+
+impl DKG {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub async fn apply(
+        &mut self,
+        data: Vec<u8>,
+    ) -> Result<bool, prost::DecodeError> {
+        let dkg_msg = Dkg::decode(&data[..])?;
+        Ok(true)
+    }
 }
