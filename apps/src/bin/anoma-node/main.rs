5045245214a5b4c9ced8be2f2058a512413274bb
Tomáš Zemanovič <tomas@heliax.dev>
2021-11-03 11:19:59 +0100

    [apps/node] add dhat heap memory profiler

--- apps/src/bin/anoma-node/main.rs
+++ apps/src/bin/anoma-node/main.rs
@@ -5,8 +5,15 @@ use std::str::FromStr;
 use anoma_apps::logging;
 use color_eyre::eyre::Result;
 use tracing_subscriber::filter::Directive;
+use dhat::{Dhat, DhatAlloc};
+
+#[global_allocator]
+static ALLOCATOR: DhatAlloc = DhatAlloc;
 
 fn main() -> Result<()> {
+    // https://docs.rs/dhat/0.2.2/dhat/#usage-heap-profiling
+    let _dhat = Dhat::start_heap_profiling();
+
     // init error reporting
     color_eyre::install()?;
 
